13fb4c570f3667c32a66b78d648d1fefae298a57
stoilsteve-hub <stoil.steve@me.com>
2026-03-22 16:25:10 +0100

    Display product details on screen

--- product.js
+++ product.js
@@ -5,7 +5,18 @@ const productId = urlParams.get('id');
 async function getProduct() {
     const res = await fetch('https://fakestoreapi.com/products/' + productId);
     const product = await res.json();
-    console.log(product);
+    
+    document.getElementById('single-product-container').innerHTML = `
+        <article class="product-card">
+            <header>
+                <h2>${product.title}</h2>
+            </header>
+            <img src="${product.image}" alt="${product.title}">
+            <p class="price">${product.price}€</p>
+            <p class="description">${product.description}</p>
+            <button id="order-btn">Order</button>
+        </article>
+    `;
 }
 
 if (productId) {
