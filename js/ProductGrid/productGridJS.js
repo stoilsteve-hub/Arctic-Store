5db804e690e054671ab7dcef3db34dfae9d117bc
CamSten <camilla.stenklov@gmail.com>
2026-03-30 13:58:58 +0200

    Added category bar for product filtering.

--- js/ProductGrid/productGridJS.js
+++ js/ProductGrid/productGridJS.js
@@ -1,10 +1,45 @@
-const productGrid = document.getElementById("productGrid");
-fetch("https://fakestoreapi.com/products")
-    .then((res) => res.json())
-    .then((products) => {
+document.addEventListener("DOMContentLoaded", () => {
+    const productGrid = document.getElementById("productGrid");
+    const categoryBar = document.getElementById("allCategoryButtons");
 
-        products.forEach(product => {
-            const card = `
+    fetch("https://fakestoreapi.com/products/categories")
+        .then((res) => res.json())
+        .then((categories) => {
+            categories.forEach(cat => {
+                const catButton = document.createElement('button');
+                catButton.type = "button";
+                catButton.className = "btn m-1 catButton";
+                catButton.textContent = cat;
+                catButton.addEventListener('click', () => {
+                    showProducts(cat)
+                });
+                categoryBar.appendChild(catButton);
+            })
+            categoryBar.appendChild(getAllButton());
+        });
+
+    showProducts('all');
+
+    function getAllButton() {
+        const catButton = document.createElement('button');
+        catButton.type = "button";
+        catButton.className = "btn m-1 catButton";
+        catButton.textContent = 'all';
+        catButton.addEventListener('click', () => {
+            showProducts('all')
+        });
+        return catButton;
+    }
+
+    function showProducts(category) {
+        fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((products) => {
+                const allProducts = getProductSubset(products, category);
+
+                productGrid.innerHTML = '';
+                allProducts.forEach(product => {
+                    const card = `
 <div class="col">
                     <a href="../../product.html?id=${product.id}" class="grid-card-link">
                         <div class="grid-card">
@@ -22,6 +57,17 @@ fetch("https://fakestoreapi.com/products")
                     </a>
   </div>
       `;
-            productGrid.innerHTML += card;
-        });
-    });
+                    productGrid.innerHTML += card;
+                });
+            });
+    }
+
+    function getProductSubset(allProducts, category) {
+        if (category == 'all') {
+            return allProducts;
+        }
+        else {
+            return allProducts.filter(p => p.category == category);
+        }
+    }
+});
